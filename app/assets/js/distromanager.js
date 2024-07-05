4309e4123f2d885e2e3ec0cfcc7cbbf8ef818d47
Timortal0 <timortal.pro@gmail.com>
2024-07-05 12:34:02 +0200

    a

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -4,7 +4,7 @@ const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
 // exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
-exports.REMOTE_DISTRO_URL = 'https://epsycraftr.000webhostapp.com/Epsycraft/distribution.json'
+exports.REMOTE_DISTRO_URL = 'https://epsycraft.wstr.fr/Epsycraft/distribution.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
